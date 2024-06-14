b3530d2dd6fb5c3fd5e6da57e9b57d40026ba059
nidak0658 <165899145+nidak0658@users.noreply.github.com>
2024-06-14 17:23:11 +0500

    Add files via upload

---
+++ TypeScript-Sir-Ameen-Alam/async.ts
@@ -0,0 +1,39 @@
+// Part 1
+/* let PubG
+PubG = "I am playing PUBG, please provide protocol AC room with Biryani and Ice Cream"
+console.log(PubG);
+function GetColdDrink(){
+    return "NHI HAI"
+}
+console.log(GetColdDrink); // NHI HAI
+(GetColdDrink)(); // (Larki Code)->(This Line) */
+
+// Part 2
+// let PubG - Declare
+/* let PubG = "I am playing PUBG, please provide protocol AC room with Biryani and Ice Cream"
+console.log(PubG, "PUBG");
+// function GetColdDrink - Declare
+function GetColdDrink(){
+    return "NHI HAI"
+}
+console.log(GetColdDrink(), "GET COLD") // NHI HAI */
+
+// Part 3
+/* function makePizza(){
+    let order = "Your Pizza is being prepared"
+    console.log(order, "Kitchen");
+    return order
+}
+
+let shahab = makePizza()
+console.log(shahab, " Table 1 ");
+let ahmed = makePizza()
+console.log(ahmed, " Table 2 "); */
+
+// Part 4
+/* function makePizza(){
+    console.log("Your Pizza is being prepared");
+}
+
+let makePizzaNew = () => "Your Pizza is being prepared"
+console.log(makePizzaNew(), " Table 3 "); */
